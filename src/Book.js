0bf2d604519a1f6df117512effa615b4385fd0fd
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-11 22:35:25 +0100

    Display form conditionally

--- src/Book.js
+++ src/Book.js
@@ -8,6 +8,26 @@ export default function Book({ book }) {
       <hr />
       <h3 className="text-center display-4">{book.title}</h3>
       <p className="lead">{book.content}</p>
+
+      {book.isLogin &&
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <div className="form-group form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      }
+
     </section>
   );
 }
