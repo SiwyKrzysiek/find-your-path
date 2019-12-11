35b3abd918518bc769ae75357c51dca4f5648f52
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-11 22:49:49 +0100

    Login form

--- src/Book.js
+++ src/Book.js
@@ -12,17 +12,12 @@ export default function Book({ book }) {
       {book.isLogin &&
         <form>
           <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label htmlFor="exampleInputEmail1">Login</label>
+            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
             <input type="password" className="form-control" id="exampleInputPassword1" />
-          </div>
-          <div className="form-group form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
