31f3c2bb3e7ef6ae86c85cfcbb0f38c44c953147
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-11 11:53:31 +0100

    Home component

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import Navigation from "./Navigation"
+
+export default function Home(props) {
+    return (
+        <div className="container">
+            <div className="row">
+
+                <Navigation>
+                    <Link to="/cellar" className="nav-link">Cellar</Link>
+                </Navigation>
+
+                <div className="card col" style={{ "width": "18rem;" }}>
+                    {/* <img src="..." className="card-img-top" alt="..." /> */}
+                    <div className="card-body">
+                        <h5 className="card-title">Home</h5>
+                        <p clasclassNames="card-text">Time to go on an adventure</p>
+                    </div>
+                </div >
+
+            </div>
+        </div>
+    );
+}
