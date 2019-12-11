2b2cb8a915e9b5459695df0f0504609da227e9f6
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-11 11:07:28 +0100

    Navigation template

--- src/App.js
+++ src/App.js
@@ -19,16 +19,41 @@ class App extends React.Component {
           <h2>Find your own path</h2>
         </section>
 
-        <div className="container">
-          <div className="card" style={{ "width": "18rem;" }}>
-            {/* <img src="..." className="card-img-top" alt="..." /> */}
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p clasclassNames="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="google.com" className="btn btn-primary">Go somewhere</a>
+        <main className="container">
+          <div className="row">
+
+            <div className="card col-3 navigation">
+              <div className="card-body">
+                <h5 className="card-title">Navigation</h5>
+                <ul class="nav flex-column">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div >
-        </div>
+
+            <div className="card col" style={{ "width": "18rem;" }}>
+              {/* <img src="..." className="card-img-top" alt="..." /> */}
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p clasclassNames="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="google.com" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div >
+
+          </div>
+        </main>
+
       </>
     );
   }
