3103c4b81cc892682d49d2e2165ec28e3f25bfd3
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-11 10:55:55 +0100

    Add page title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logged: false
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <section className="container logo">
+          <h2>Find your own path</h2>
+        </section>
+
+        <div className="container">
+          <div className="card" style={{ "width": "18rem;" }}>
+            {/* <img src="..." className="card-img-top" alt="..." /> */}
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p clasclassNames="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="google.com" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div >
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
